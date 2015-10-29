14573bbda82fd4425a5c52dadbfaf52d2e0f6c22
sjoshi6 <sjoshi6@ncsu.edu>
2015-10-29 17:53:51 -0400

    Final Card Game commit

--- CardGame/CardGame/ViewController.swift
+++ CardGame/CardGame/ViewController.swift
@@ -16,13 +16,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCardImageView: UIImageView!
     @IBOutlet weak var dealButton: UIButton!
     
+    @IBOutlet weak var player: UILabel!
+    @IBOutlet weak var enemy: UILabel!
+    
+    //MARK: Logic Vars
+    var playerScore:Int = 0
+    var enemyScore:Int = 0
+    var cardNameArray:[String] = ["card2", "card3", "card4", "card5", "card6", "card7",
+        "card8", "card9", "card10", "jack", "queen", "king", "ace"]
     
     //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Changing the button text in normal mode
-        self.dealButton.setTitle("Play", forState: UIControlState.Normal)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -32,10 +37,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: App Logic
     @IBAction func dealButtonAction(sender: UIButton) {
         
-        self.firstCardImageView.image = UIImage(named: "card12")
-        self.secondCardImageView.image = UIImage(named: "card1")
+        // Generating two random numbers for cards
+        let firstRanNum:Int = Int(arc4random_uniform(13))
+        let secondRanNum:Int = Int(arc4random_uniform(13))
+        
+        // constructing strings with card%i format
+        let firstRanStr = cardNameArray[firstRanNum]
+        let secondRanStr = cardNameArray[secondRanNum]
+        
+        // setting image view to the new card str named cards
+        self.firstCardImageView.image = UIImage(named: firstRanStr)
+        self.secondCardImageView.image = UIImage(named: secondRanStr)
+    
+    
+        if firstRanNum > secondRanNum {
+            
+            self.playerScore += 1
+            self.player.text = String(self.playerScore)
+        }
+        else if firstRanNum == secondRanNum {
+            
+        }
+        else {
+            
+            self.enemyScore += 1
+            self.enemy.text = String(self.enemyScore)
+        }
+        
+        
     }
 
 }
